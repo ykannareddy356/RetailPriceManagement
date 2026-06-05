6e4c6fb89c2dba012956b496f40060e13a607a17
Yerram Kanna Venkateswara Reddy <ykannareddy356@gmail.com>
2026-06-05 12:09:08 +0530

    fix root route redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        <Route path="/" element={<Navigate to="/login" />} />
+
         <Route path="/login" element={<Login />} />
 
         <Route
