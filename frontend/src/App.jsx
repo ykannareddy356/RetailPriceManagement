2bb3f00d07e78d77fb5d1ab79d29ea7f6a3e902e
Yerram Kanna Venkateswara Reddy <ykannareddy356@gmail.com>
2026-06-05 12:13:44 +0530

    fix navigate import issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import PrivateRoute from "./components/PrivateRoutes";
